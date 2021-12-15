16e2d5b1569577c89cc5e9eb0302fc246bf0b0d8
vanya12042 <vanya.streetworkout.sw.vol@gmail.com>
2021-12-16 00:18:38 +0200

    Finished

--- lesson_8/additional/main.js
+++ lesson_8/additional/main.js
@@ -1,17 +1,45 @@
 // написати рекурсивну функцію, яка збирає всі назви класів з файлу rules.html в окремий масив. масив вивести в консоль
 
-// let arr = [];
-//
-// function reCall(startElement) {
-//     if (startElement.classList.value) {
-//         arr.push(startElement.classList.value);
-//     }
-//     if (startElement.children.length) {
-//         for (const element of startElement.children) {
-//             reCall(element);
-//         }
-//     }
-// }
-// reCall(document.body);
-//
-// console.log(arr.join(' ').split(' '));
+
+function classesHtml(startElementHtml) {
+
+    let classesHtml = [];
+
+    reCall(startElementHtml);
+
+    function reCall(startElement) {
+
+        if (startElement.classList.value) {
+            classesHtml.push(startElement.classList.value);
+        }
+
+
+        if (startElement.children.length) {
+            for (const element of startElement.children) {
+                reCall(element);
+            }
+        }
+    }
+
+    classesHtml = classesHtml.join(' ').split(' ');
+
+    let count = 0;
+    for (let i = 0; i < classesHtml.length; i++) {
+        for (let j = 0; j < classesHtml.length; j++) {
+            if (classesHtml[i] === classesHtml[j]) {
+                count++;
+            }
+        }
+        if (count > 1) {
+            classesHtml.splice(i, 1);
+        }
+
+        count = 0;
+    }
+
+    return classesHtml;
+
+}
+
+
+console.log(classesHtml(document.body));
